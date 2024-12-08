59f3ea2db0b358b95242228da4ffa88457197b0e
Francesco <tomaselli.fr@gmail.com>
2024-12-08 13:19:15 +0100

    Update

--- README.md
+++ README.md
@@ -35,10 +35,11 @@ Here is the complete list of solutions:
 
 | **Day** | **Link** | **Total Lines** | **Effective Lines**| **Runtime** | **Last Updated** |
 | -: | - | -: | -: | -: | -: |
-| 01 | [Link](./src/aoc/solutions/01.py) |       39 | 26 | 0m0.078s | Thu, Dec 05, 2024 |
-| 02 | [Link](./src/aoc/solutions/02.py) |       44 | 30 | 0m0.075s | Thu, Dec 05, 2024 |
-| 03 | [Link](./src/aoc/solutions/03.py) |       49 | 33 | 0m0.071s | Thu, Dec 05, 2024 |
-| 04 | [Link](./src/aoc/solutions/04.py) |       66 | 42 | 0m0.102s | Thu, Dec 05, 2024 |
-| 05 | [Link](./src/aoc/solutions/05.py) |       62 | 43 | 0m0.071s | Thu, Dec 05, 2024 |
-| 06 | [Link](./src/aoc/solutions/06.py) |      101 | 65 | 0m2.705s | Fri, Dec 06, 2024 |
-| 07 | [Link](./src/aoc/solutions/07.py) |       46 | 30 | 0m0.654s | Sat, Dec 07, 2024 |
+| 01 | [Link](./src/aoc/solutions/01.py) |       39 | 26 | 0m0.071s | Thu, Dec 05, 2024 |
+| 02 | [Link](./src/aoc/solutions/02.py) |       44 | 30 | 0m0.072s | Thu, Dec 05, 2024 |
+| 03 | [Link](./src/aoc/solutions/03.py) |       49 | 33 | 0m0.070s | Thu, Dec 05, 2024 |
+| 04 | [Link](./src/aoc/solutions/04.py) |       66 | 42 | 0m0.101s | Thu, Dec 05, 2024 |
+| 05 | [Link](./src/aoc/solutions/05.py) |       62 | 43 | 0m0.072s | Thu, Dec 05, 2024 |
+| 06 | [Link](./src/aoc/solutions/06.py) |      101 | 65 | 0m2.706s | Fri, Dec 06, 2024 |
+| 07 | [Link](./src/aoc/solutions/07.py) |       46 | 30 | 0m0.638s | Sat, Dec 07, 2024 |
+| 08 | [Link](./src/aoc/solutions/08.py) |       73 | 46 | 0m0.338s | Sun, Dec 08, 2024 |
---
+++ src/aoc/solutions/08.py
@@ -0,0 +1,73 @@
+from aoc.utilities.fetch import get_input
+from collections import defaultdict
+
+
+def coefficient(x1, y1, x2, y2):
+    dx = x1 - x2
+    dy = y1 - y2
+
+    if dx == 0:
+        return float("inf")
+
+    return dy / dx
+
+
+def distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def first_check(xr, yr, x1, y1, x2, y2):
+    if coefficient(xr, yr, x1, y1) != coefficient(xr, yr, x2, y2):
+        return False
+
+    dist_a = distance(xr, yr, x1, y1)
+    dist_b = distance(xr, yr, x2, y2)
+    return dist_a == dist_b * 2 or dist_a * 2 == dist_b
+
+
+def second_check(xr, yr, x1, y1, x2, y2):
+    return coefficient(xr, yr, x1, y1) == coefficient(xr, yr, x2, y2)
+
+
+def find_nodes(n, m, point_a, point_b, valid_check):
+    res = set()
+
+    for i in range(n):
+        for j in range(m):
+            if valid_check(i, j, *point_a, *point_b):
+                res.add((i, j))
+
+    return res
+
+
+def compute_antinodes(n, m, coordinates, valid_check):
+    res = set()
+
+    for i in range(len(coordinates)):
+        for j in range(i + 1, len(coordinates)):
+            res |= find_nodes(n, m, coordinates[i], coordinates[j], valid_check)
+
+    return res
+
+
+def solve_all(data):
+    d = defaultdict(list)
+    n, m = len(data), len(data[0])
+
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == ".":
+                continue
+            d[data[i][j]].append((i, j))
+
+    ans1, ans2 = set(), set()
+
+    for v in d.values():
+        ans1 |= compute_antinodes(n, m, v, first_check)
+        ans2 |= compute_antinodes(n, m, v, second_check) | set(v)
+
+    print(f"{len(ans1)}\n{len(ans2)}")
+
+
+data = list(map(list, get_input(8).splitlines()))
+solve_all(data)
